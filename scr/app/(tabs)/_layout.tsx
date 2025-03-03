f7cc13ef3a75f8e01a03588d57976d62744c0e32
Viggo Kirkegaard Helstrup <vkirkegaardhelstrup@gmail.com>
2025-03-03 11:26:24 +0100

    tabs work

--- scr/app/(tabs)/_layout.tsx
+++ scr/app/(tabs)/_layout.tsx
@@ -1,11 +1,12 @@
 import { Stack, Tabs } from "expo-router";
 
 export default function TabLayout() {
-  return (<Stack>
+  return (
+  <Tabs>
     <Tabs.Screen name="index" options={{ title: 'Home' }} />
     <Tabs.Screen name="friends" options={{ title: 'Friends' }} />
     <Tabs.Screen name="local" options={{ title: 'Local' }} />
     <Tabs.Screen name="memories" options={{ title: 'Memories' }} />
-  </Stack>
+  </Tabs>
   );
 }
